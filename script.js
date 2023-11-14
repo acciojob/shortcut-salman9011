014d056ea152bdb93c8a7de83092f2bb1b0259b2
salman <70580281+salman9011@users.noreply.github.com>
2023-11-14 22:57:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+// Modify the function to use the Cypress prompt stub
 function shortcut(s1, s2) {
   // Check if either of the input strings is empty
   if (s1 === "" || s2 === "") {
@@ -9,6 +10,17 @@ function shortcut(s1, s2) {
 
   return initialLetters;
 }
+
+// Use Cypress commands to handle prompts and alerts
+cy.visit(baseUrl, {
+  onBeforeLoad(win) {
+    cy.stub(win, "prompt").onFirstCall().returns("Amnesty").onSecondCall().returns("International");
+  },
+}).then(() => {
+  cy.on("window:alert", str => {
+    expect(str).to.equal("AI");
+  });
+});
 
 // Do not change the code below.
 const s1 = prompt("Enter s1:");
